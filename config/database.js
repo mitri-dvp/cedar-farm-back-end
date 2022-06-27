dfa426ceeef40d18620ba1cd0e93f92c036d1679
mitri-dvp <mitri.dvp@gmail.com>
2022-06-27 03:10:06 -0400

    Updated server configuration

--- config/database.js
+++ config/database.js
@@ -7,7 +7,9 @@ module.exports = ({ env }) => ({
       database: env('DATABASE_NAME', 'cedar-farm'),
       user: env('DATABASE_USERNAME', ''),
       password: env('DATABASE_PASSWORD', ''),
-      ssl: env.bool('DATABASE_SSL', false),
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
   },
 });
