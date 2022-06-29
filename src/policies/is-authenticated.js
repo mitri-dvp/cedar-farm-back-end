f78cf64b1d0902f89359c16578cd527da0586d5a
mitri-dvp <mitri.dvp@gmail.com>
2022-06-28 23:34:50 -0400

    Updated URL parsing

--- src/policies/is-authenticated.js
+++ src/policies/is-authenticated.js
@@ -1,8 +1,5 @@
 module.exports = (ctx, config, { strapi }) => {
-  console.log('Request:', ctx.request);
-  console.log('Header Host:', ctx.request.headers.host);
-  console.log('Request URL:', ctx.request.url);
-  const url = new URL(ctx.request.headers.host + ctx.request.url);
+  const url = new URL('https://example.com' + ctx.request.url);
   const key = url.searchParams.get('key');
   return key === process.env.API_KEY ? true : false;
 };
