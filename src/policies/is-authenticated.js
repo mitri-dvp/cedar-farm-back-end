80553afcb95ce738d856b77969b88f9eeede3463
mitri-dvp <mitri.dvp@gmail.com>
2022-06-28 23:14:21 -0400

    URL console log added

--- src/policies/is-authenticated.js
+++ src/policies/is-authenticated.js
@@ -1,4 +1,7 @@
 module.exports = (ctx, config, { strapi }) => {
+  console.log('Request:', ctx.request);
+  console.log('Header Host:', ctx.request.headers.host);
+  console.log('Request URL:', ctx.request.url);
   const url = new URL(ctx.request.headers.host + ctx.request.url);
   const key = url.searchParams.get('key');
   return key === process.env.API_KEY ? true : false;
